109709c914621784c5b1a065df4ce19d7eee4353
kthayalan <kthayalan02@gmail.com>
2022-11-11 23:32:25 +0530

    APIView to ViewSet

--- AddressComponent/urls.py
+++ AddressComponent/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from AddressComponent.views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'address', AddressViewSet)
 
 urlpatterns = [
-    path('address/', AddressViewAPI.as_view(), name='address'),
+    path('', include(router.urls))
 ]
